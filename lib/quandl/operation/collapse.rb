5b1579c262ea050d2d6189901143ed99ce72ef67
RaymondMcT <raymond@quandl.com>
2014-04-25 20:03:26 -0400

    initial changes, not operational

--- lib/quandl/operation/collapse.rb
+++ lib/quandl/operation/collapse.rb
@@ -9,6 +9,7 @@ class Collapse
   class << self
   
     def perform(data, type)
+      type = {:freq => type} if type.class != Hash
       assert_valid_arguments!(data, type)
       # nothing to do with an empty array
       return data unless data.compact.present?
@@ -17,7 +18,7 @@ class Collapse
       # operations expect data in ascending order
       data = Sort.asc(data)
       # collapse
-      data = collapse(data, type)
+      data = collapse(data, type[:freq], type[:algo])
       # return to original order
       data = Sort.desc(data) if order == :desc
       # onwards
@@ -26,7 +27,8 @@ class Collapse
     
     def assert_valid_arguments!(data, type)
       raise ArgumentError, "data must be an Array. Received: #{data.class}" unless data.is_a?(Array)
-      raise ArgumentError, "frequency must be one of #{valid_collapses}. Received: #{type}" unless valid?(type)
+      raise ArgumentError, "frequency must be one of #{valid_collapses}. Received: #{type[:freq]}" unless valid?(type[:freq])
+      raise ArgumentError, "algorithm must be one of #{valid_collapse_algos}. Received: #{type[:algo]}" unless type[:algo].nil? || valid_collapse_algo?(type[:algo])
     end
     
     def valid_collapse?(type)
@@ -40,32 +42,149 @@ class Collapse
     def valid_collapses
       [ :daily, :weekly, :monthly, :quarterly, :annual ]
     end
+
+    def valid_collapse_algos
+      [ :min, :max, :first, :last, :mid, :sum, :avg, :median ]
+    end
+
+    def valid_collapse_algo?(type)
+      valid_collapse_algos.include?( type.try(:to_sym) )
+    end
+
   
-    def collapse(data, frequency)
+    def collapse(data, frequency, algorithm)
+      algorithm ||= :last
       return data unless valid_collapse?( frequency )
+      return data unless valid_collapse_algo?( algorithm )
       # store the new collapsed data
       collapsed_data = {}
-      range = find_end_of_range( data[0][0], frequency )
-      # iterate over the data
-      data.each do |row|
-        # grab date and values
-        date, value = row[0], row[1..-1]
-        value = value.first if value.count == 1
-        # bump to the next range if it exceeds the current one
-        range = find_end_of_range(date, frequency) unless inside_range?(date, range)
-        # consider the value for the next range
-        if inside_range?(date, range) && value.present?
-          # merge this and previous row if nils are present
-          value = merge_row_values( value, collapsed_data[range] ) unless collapsed_data[range].nil?
-          # assign value
-          collapsed_data[range] = value
+      data_in_range = {}
+      
+      case algorithm.to_sym
+      when :min
+        range = get_range( data[0][0], frequency )
+        data.each do |row|
+          date, value = row[0], row[1..-1]
+          range = get_range(date, frequency) unless inside_range?(date, range)
+          collapsed_data[range] ||= value
+          value.each_index do |a|
+            collapsed_data[range][a] = value[a] if (collapsed_data[range][a].nil?  && !value[a].nil?) || collapsed_data[range][a] > value[a]
+          end
         end
+        return(to_table(collapsed_data))
       end
-      to_table(collapsed_data)
+
+      when :max
+        range = get_range( data[0][0], frequency )
+        data.each do |row|
+          date, value = row[0], row[1..-1]
+          range = get_range(date, frequency) unless inside_range?(date, range)
+          collapsed_data[range] ||= value
+          value.each_index do |a|
+            collapsed_data[range][a] = value[a] if (collapsed_data[range][a].nil?  && !value[a].nil?) || collapsed_data[range][a] < value[a]
+          end
+        end
+        return(to_table(collapsed_data))
+      when :first
+        range = get_range( data[-1][0], frequency )
+        Sort.desc(data).each do |row|
+          # grab date and values
+          date, value = row[0], row[1..-1]
+          value = value.first if value.count == 1
+          # bump to the next range if it exceeds the current one
+          range = get_range(date, frequency) unless inside_range?(date, range)
+          # consider the value for the next range
+          if inside_range?(date, range) && value.present?
+            # merge this and previous row if nils are present
+            value = merge_row_values( value, collapsed_data[range] ) unless collapsed_data[range].nil?
+            # assign value
+            collapsed_data[range] = value
+          end
+        end
+        return(to_table(collapsed_data))
+      when :last
+        range = find_end_of_range( data[0][0], frequency )
+        data.each do |row|
+          # grab date and values
+          date, value = row[0], row[1..-1]
+          value = value.first if value.count == 1
+          # bump to the next range if it exceeds the current one
+          range = find_end_of_range(date, frequency) unless inside_range?(date, range)
+          # consider the value for the next range
+          if inside_range?(date, range) && value.present?
+            # merge this and previous row if nils are present
+            value = merge_row_values( value, collapsed_data[range] ) unless collapsed_data[range].nil?
+            # assign value
+            collapsed_data[range] = value
+          end
+        end
+        return(to_table(collapsed_data))
+      when :mid
+      when :sum
+        range = get_range( data[0][0], frequency )
+        data.each do |row|
+          date, value = row[0], row[1..-1]
+          range = get_range(date, frequency) unless inside_range?(date, range)
+          data_in_range[range] ||= []
+          data_in_range[range] << value
+        end
+        data_in_range.each do |range, values|
+          sum = Array.new(values.first.length, 0)
+          values.each do |value|
+            value.each_index do |index|
+              sum[index] += value[index] unless value[index].nil?
+            end
+          end
+          collapsed_data[range] = sum
+        end
+        return(to_table(collapsed_data))
+      when :avg
+        range = get_range( data[0][0], frequency )
+        data.each do |row|
+          date, value = row[0], row[1..-1]
+          range = get_range(date, frequency) unless inside_range?(date, range)
+          data_in_range[range] ||= []
+          data_in_range[range] << value
+        end
+        data_in_range.each do |range, values|
+          avg = Array.new(values.first.length, 0)
+          counter = Array.new(values.first.length, 0)
+          values.each do |value|
+            value.each_index do |index|
+              avg[index] += value[index] unless value[index].nil?
+              counter[index] += 1 unless value[index].nil?
+            end
+          end
+          avg.each_index do |index|
+            avg[index] = counter[index] == 0 ? nil : avg[index]/counter[index]
+          end
+          collapsed_data[range] = avg
+        end
+        return(to_table(collapsed_data))
+      when :median
+        # data_in_range.each do |range, values|
+        #   avg = Array.new(values.first.length, 0)
+        #   counter = Array.new(values.first.length, 0)
+        #   values.each do |value|
+        #     value.each_index do |index|
+        #       avg[index] += value[index] unless value[index].nil?
+        #       counter[index] += 1 unless value[index].nil?
+        #     end
+        #   end
+        #   avg.each_index do |index|
+        #     avg[index] = counter[index] == 0 ? nil : avg[index]/counter[index]
+        #   end
+        #   collapsed_data[range] = avg
+        # end
+        # return(to_table(collapsed_data))
+
+      # iterate over the data
+      
     end
     
     def to_table(data)
       data.collect do |date, values|
+        date = date[1] if date.is_a?(Array)
         if values.is_a?(Array)
           values.unshift(date)
         else
@@ -101,11 +220,23 @@ class Collapse
     end
   
     def inside_range?(date, range)
-      date <= range
+      if range.is_a?(Array)
+        range[0] <= date && date <= range[1]
+      else
+        date <= range
+      end
+    end
+
+    def get_range(date, frequency)
+      [date.start_of_frequency(frequency), date.end_of_frequency(frequency)]
     end
   
     def find_end_of_range(date, frequency)
       date.end_of_frequency(frequency)
+    end
+
+    def find_start_of_range(date, frequency)
+      date.start_of_frequency(frequency)
     end
   
     def find_each_index(array, find)
